e6495b5fc10a84d65543195abd7cdceb590b6071
Karol Orszulik <orkarox@gmail.com>
2022-02-02 22:20:49 +0100

    Added support for changing nicknames and storing them on the server; recieving new room ID upon connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log("PaintOnline starting...")
+console.log("PaintOnline starting...\n")
 
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const server = app.listen(3000)
 
@@ -10,9 +11,30 @@ const socket = require('socket.io')
 const io = socket(server)
 
 
+let clientsNicknames = {}
+
 
 io.sockets.on('connection', (socket) => {
 
-    console.log(`New connection with ID: ${socket.id}`)
+    const newID = generateRoomID(4)
+    console.log(`New connection with ID: ${socket.id}; Assigning room ID: ${newID}`)
+    socket.emit('new-room-id', {id:newID})
+
+    clientsNicknames[socket.id] = 'Joe Generic'
+
+    socket.on('changed-nickname', (data) => {
+        clientsNicknames[socket.id] = data.newNickname
+        console.log(`Socket ${socket.id} now has nickname: ${data.newNickname}`)
+    })
 
 })
+
+const generateRoomID = (length, taken) => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let result = ""
+
+    for(let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random()*characters.length)]
+    }
+    return result
+}
--- public/script.js
+++ public/script.js
@@ -1,5 +1,24 @@
 const socket = io("http://localhost:3000")
 
+// upon recieving the ID of a new room
+// when connecting for the first time,
+// show the new ID
+socket.on('new-room-id', (data) => {
+    document.getElementById('room-id').innerText = data.id
+})
+
+// when new nickname buttons is pressed
+// change the displayed nickname and
+// emit the change to the server
+document.getElementById('nickname-btn').onclick = () => {
+    const newNickname = document.getElementById('nickname-input').value
+    if(newNickname != '') {
+        document.getElementById('nickname').innerText = newNickname
+    }
+    
+    socket.emit('changed-nickname', {newNickname})
+}
+
 function setup() {
     createCanvas(800,600).parent(select('main'))
     background(0);
