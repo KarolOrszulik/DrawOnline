fbb5cfdf0806f0153edf4fcdaaad898d879fdca5
Karol Orszulik <orkarox@gmail.com>
2022-02-02 18:17:15 +0100

    Serving the frontend and establishing a websocket

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("PaintOnline starting...")
+
+const express = require('express')
+const app = express()
+const server = app.listen(3000)
+
+app.use(express.static('public'))
+
+const socket = require('socket.io')
+const io = socket(server)
+
+
+
+io.sockets.on('connection', (socket) => {
+
+    console.log(`New connection with ID: ${socket.id}`)
+
+})
