4c244d72fa116d59ae3cc4039d88ffdbceca2942
Karol Orszulik <orkarox@gmail.com>
2022-02-06 18:58:34 +0100

    Added comments to code

--- index.js
+++ index.js
@@ -1,83 +1,118 @@
 console.log("PaintOnline starting...\n")
 
+// set up express to host files in 'public' directory on port 3000
 const express = require('express')
-const res = require('express/lib/response')
 const app = express()
 const server = app.listen(3000)
-
 app.use(express.static('public'))
 
+// set up socket.io on the same port as server
 const socket = require('socket.io')
 const io = socket(server)
 
+// send a message with current user count every 5s
+let userCount = 0
 setInterval(() => {io.emit('update-user-count', userCount)}, 5000)
 
-let userCount = 0
-
+// dictionary to relate socket IDs with users' nicknames
 let clientsNicknames = {}
 clientsNicknames.log = () => console.log(`Clients-nicknames dict: ${JSON.stringify(clientsNicknames)}`)
 
+// when recieving a new socket connection...
 io.sockets.on('connection', (socket) => {
 
+    // increment current user count
     userCount++
 
+    // generate new room ID and nickname for the new socket
     const newID = generateRoomID(4, Array.from(socket.adapter.rooms.keys()))
     const newNickname = generateNewNickname()
-
     console.log(`New socket ID: ${socket.id}; Room ID: ${newID}; Nickname: ${newNickname}`)
 
+    // assign the nickname to the socket's ID
     clientsNicknames[socket.id] = newNickname
+
+    // save the socket's new room ID and have it join a room of that ID
     socket.room = newID
     socket.join(socket.room)
 
+    // emit a message to the new socket with all initial info
     socket.emit('init', {id:newID, nickname:newNickname, userCount})
 
+    // handle user changing their nickname
     socket.on('changed-nickname', (newNickname) => {
+        // update the entry in the dictionary
         clientsNicknames[socket.id] = newNickname
         console.log(`Socket ${socket.id} now has nickname: ${newNickname}`)
-        clientsNicknames.log()
+        
+        // inform all users in the same room of the change
         emitUpdateNicknamesList(socket.room)
     })
 
+    // handle user wantin to join a room
     socket.on('join-room-request', (roomToJoin) => {
+
+        // establish if the room exists
         const roomToJoinExists = socket.adapter.rooms.has(roomToJoin)
+
         if(roomToJoinExists) {
             console.log(`Client ${socket.id} wants to join an EXISTING room ${roomToJoin}`) 
 
+            // leave the old room, save the new room ID and join the room
             socket.leave(socket.room)
             socket.room = roomToJoin
             socket.join(socket.room)
 
-            const nicknamesInRoom = getNicknamesInRoom(roomToJoin)
-
-            socket.emit('join-room-greenlight', {roomToJoin, nicknamesInRoom})
+            // allow the socket to join room, update all user lists in the room
+            socket.emit('join-room-greenlight', roomToJoin)
             emitUpdateNicknamesList(socket.room)
         }
         else {
             console.log(`Client ${socket.id} wants to join an NON-EXISTING room ${roomToJoin}`)
-            socket.emit('join-room-error')
+            socket.emit('join-room-error', roomToJoin)
         }
     })
 
+    // handle user disconnecing
     socket.on('disconnect', (data) => {
+        // decrement user count
         userCount--
+        
+        // remove the entry from the dictionary
         delete clientsNicknames[socket.id]
+
+        // update all user lists in the room
         emitUpdateNicknamesList(socket.room)
+
         console.log(`${socket.id} a.k.a. ${clientsNicknames[socket.id]} disconnected`)
     })
 
-})
+}) // end of socket.on('connection')
 
+
+// emits all users' nicknames to all sockets in a given room
 const emitUpdateNicknamesList = (roomID) => {
-    io.to(roomID).emit('update-nicknames-list', getNicknamesInRoom(roomID))
+
+    const room = io.sockets.adapter.rooms.get(roomID)
+
+    // only continue if room exists (not all users have disconnected)
+    if (room) { 
+        const nicknames = Array.from(room.values()).map(x => clientsNicknames[x])
+        io.to(roomID).emit('update-nicknames-list', nicknames)
+    }
 }
 
+
+// returns an array of all users' nicknames in a given room
 const getNicknamesInRoom = (roomID) => {
     const room = io.sockets.adapter.rooms.get(roomID)
     if (room)
         return Array.from(room.values()).map(x => clientsNicknames[x])
 }
 
+
+// returns a string of [length] random capital letters
+// that doesn't appear in the [taken] Set
 const generateRoomID = (length, taken) => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -94,6 +129,7 @@ const generateRoomID = (length, taken) => {
 }
 
 
+// generate random nickname of format "{adjective} {noun}"
 const generateNewNickname = () => {
 
     // arrays from https://stackoverflow.com/questions/7666516/fancy-name-generator-in-node-js/7668656
--- public/script.js
+++ public/script.js
@@ -1,8 +1,6 @@
 const socket = io("http://localhost:3000")
 
-// upon recieving the ID of a new room
-// when connecting for the first time,
-// show the new ID
+// initial data from server: new room ID, nickname and current user count
 socket.on('init', (data) => {
     document.getElementById('room-id').innerText = data.id
     document.getElementById('nickname').innerText = data.nickname
@@ -10,55 +8,67 @@ socket.on('init', (data) => {
     updateNicknamesList([data.nickname])
 })
 
-socket.on('join-room-greenlight', (data) => {
-    document.getElementById('room-id').innerText = data.roomToJoin
-    console.log(data.nicknamesInRoom)
-    updateNicknamesList(data.nicknamesInRoom)
+// handle joining the room as requested
+socket.on('join-room-greenlight', (roomID) => {
+    document.getElementById('room-id').innerText = roomID
 })
 
+// call updateNicknamesList()
 socket.on('update-nicknames-list', (users) => {
     updateNicknamesList(users)
 })
 
+// call updateUserCount()
 socket.on('update-user-count', (count) => {
     updateUserCount(count)
 })
 
-const updateUserCount = (count) => {
-    document.getElementById('user-count').innerText = count
-}
-
-const updateNicknamesList = (clients) => {
+// updates the list of users in room
+const updateNicknamesList = (nicknames) => {
     const list = document.getElementById('users-list')
+    
+    // clear the list
     list.innerHTML = ''
-    for(let client of clients) {
+
+    // add all nicknames as <li> elements
+    for(let nickname of nicknames) {
         let li = document.createElement('li')
-        let text = document.createTextNode(client)
+        let text = document.createTextNode(nickname)
         li.appendChild(text)
         list.appendChild(li)
     }
 }
 
-// when new nickname button is pressed
-// change the displayed nickname and
-// emit the change to the server
+// updates the current user count
+const updateUserCount = (count) => {
+    document.getElementById('user-count').innerText = count
+}
+
+
+// change nickname and inform the server of it
 document.getElementById('nickname-btn').onclick = () => {
     const newNickname = document.getElementById('nickname-input').value
     const currentNickname = document.getElementById('nickname').innerText
+
+    // ignore if input field is empty or the same as current nickname
     if(newNickname != '' && newNickname != currentNickname) {
         document.getElementById('nickname').innerText = newNickname
         socket.emit('changed-nickname', newNickname)
     }
 }
 
+// ask the server to join room of ID specified in the input field
 document.getElementById('join-room-btn').onclick = () => {
     const roomToJoin = document.getElementById('join-room-input').value.toUpperCase()
     const currentRoom = document.getElementById('room-id').innerText
+
+    // ignore if input field is empty or the same as current nickname
     if(roomToJoin != '' && roomToJoin != currentRoom) {
         socket.emit('join-room-request', roomToJoin)
     }
 }
 
+// p5.js setup function
 function setup() {
     createCanvas(800,600).parent(select('main'))
     background(0);
